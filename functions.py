c88a165adeb504c6a1decfdc2e2364ed27ad19bf
scottjstewart <scottstewart965@gmail.com>
2019-10-23 11:27:28 -0400

    finished loops, functions, and conditionals lessons and examples

--- functions.py
+++ functions.py
@@ -1,7 +1,31 @@
 # A function is a block of code which only runs when it is called. In Python, we do not use parentheses and curly brackets, we use indentation with tabs or spaces
 
 
+# create a function default value can be set and will work without receiving a parameter on function call
+def sayHello(name='sam'):
+    print(f'Hello {name}')
+
+
+sayHello()
+
+
+# Return Values
+def getSum(num1, num2):
+    total = num1 + num2
+    return total
+
+
+num = getSum(1, 2)
+print(num)
 
 # A lambda function is a small anonymous function.
 # A lambda function can take any number of arguments, but can only have one expression. Very similar to JS arrow functions
 
+# prettier reformatted the lambda function
+# getSum = lambda num1, num2 : num1 + num2
+
+
+def getSum(num1, num2): return num1 + num2
+
+
+print(getSum(10, 3))
--- loops.py
+++ loops.py
@@ -1,5 +1,32 @@
 # A for loop is used for iterating over a sequence (that is either a list, a tuple, a dictionary, a set, or a string).
 
+people = ['scott', 'nick', 'rob', 'andrew']
 
+# simple loop
+for person in people:
+    print(f'current person: {person}')
+
+# Break this stops at rob
+for person in people:
+    if person == 'rob':
+        break
+    print(f'current person: {person}')
+
+# continue this skips rob
+for person in people:
+    if person == 'rob':
+        continue
+    print(f'current person: {person}')
+
+# range
+for i in range(len(people)):
+    print(people[i])
+
+for i in range(0, 11):
+    print(f'Number: {i}')
 
 # While loops execute a set of statements as long as a condition is true.
+count = 0
+while count <= 10:
+    print(f'Count: {count}')
+    count += 1
