30e67c8d46faef7a674228192bba65f2f39b56ad
scottjstewart <scottstewart965@gmail.com>
2019-10-23 12:07:29 -0400

    added examples and lessons for files.py and py_json

--- files.py
+++ files.py
@@ -1,1 +1,26 @@
 # Python has functions for creating, reading, updating, and deleting files.
+
+
+# open a file
+my_file = open('myfile.txt', 'w')
+
+# get some info on this file
+print('Name: ', my_file.name)
+print('Is Closed: ', my_file.closed)
+print('Opening Mode: ', my_file.mode)
+
+# write to file
+my_file.write('Whaddup dude')
+my_file.write('how are you')
+my_file.close()
+
+
+# append to file
+my_file = open('myFile.txt', 'a')
+my_file.write(' its been awhile')
+my_file.close()
+
+# read from a file
+my_file = open('myFile.txt', 'r+')
+text = my_file.read(100)
+print(text)
--- py_json.py
+++ py_json.py
@@ -1,1 +1,17 @@
-# JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary
+# JSON is commonly used with data APIS. Here how we can parse JSON into a Python dictionary this file was named py_json because it cant be named the same as an import
+import json
+
+# sample json
+userJSON = '{"first_name": "Scott", "last_name":"Stewart", "age":25}'
+
+# parse to dict
+user = json.loads(userJSON)
+print(user)
+print(user['first_name'])
+
+# turn dictionary into json
+car = {'make': 'bmw', 'model': '328xi', 'year': 2007}
+
+carJSON = json.dumps(car)
+
+print(carJSON)
